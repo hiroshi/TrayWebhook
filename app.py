8e2291a66f832d3c5c8f203b225ecb7d23dbc244
Hiroshi Saito <hiroshi3110@gmail.com>
2014-10-04 22:04:35 +0900

    Added log output for webhook.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import hmac, threading, os, logging, json
+from hashlib import sha256
 from flask import Flask, request
 
 app = Flask(__name__)
-app.debug = True
+app.debug = bool(os.environ.get('DEBUG', False))
+app.logger.info("debug=%s", app.debug)
 
 @app.route("/")
 def hello():
@@ -9,4 +12,18 @@ def hello():
 
 @app.route("/webhook", methods=['GET'])
 def webhook():
-    return request.args.get('challenge') or ""
+    # return request.args.get('challenge') or "" # verify
+    # Make sure this is a valid request from Dropbox
+    if not app.debug:
+        signature = request.headers.get('X-Dropbox-Signature')
+        if signature != hmac.new(APP_SECRET, request.data, sha256).hexdigest():
+            abort(403)
+    data = json.loads(request.data)
+    app.logger.info(data)
+    # for uid in data['delta']['users']:
+        # We need to respond quickly to the webhook request, so we do the
+        # actual work in a separate thread. For more robustness, it's a
+        # good idea to add the work to a reliable queue and process the queue
+        # in a worker process.
+        # threading.Thread(target=process_user, args=(uid,)).start()
+    return ''
