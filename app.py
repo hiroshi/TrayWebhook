8e5ee51668123bc640908c03a61867f350dd310b
Hiroshi Saito <hiroshi3110@gmail.com>
2014-10-04 21:34:56 +0900

    Return challenge to verify webhook.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
 def hello():
     return "Hello World 2"
 
-@app.route("/webhook")
+@app.route("/webhook", methods=['GET'])
 def webhook():
-    return "webhook"
+    return request.args.get('challenge') or ""
